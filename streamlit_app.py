0b9f7b4f6e9b8bdadd9ccb70f6d07dd8ad7a6b1f
Andrew <116625003+Pokedrew16@users.noreply.github.com>
2024-08-21 17:04:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ if st.button('Click me'):
     if weight == "" or height == "":
         st.write("Please enter your height and weight.")
     else:
-         bmi = float(weight) / (float(height)^2)
+         bmi = float(weight) / (float(height)**2)
          bmi = round(bmi,2)
     st.write(
     f"Your BMI is {bmi}."
