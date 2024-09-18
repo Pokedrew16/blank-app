60770315e8bfd511b5b04d332254611290000fb3
Andrew <116625003+Pokedrew16@users.noreply.github.com>
2024-09-18 17:54:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ image = st.camera_input("Take a photo")
 if image is not None:
   bytes_data = image.getvalue()
   st.image(bytes_data)
-  float_array = np.frombuffer(bytes_data, dtype=np.float32)
+  float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
   st.write(float_array)
 
 
