5d865b74f80ad8de04f34a1863af9f3f4fe834dd
Andrew <116625003+Pokedrew16@users.noreply.github.com>
2024-09-18 17:49:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy
 import random
 st.title("♻️ Welcome to our responsible helper app ✅")
 image = st.camera_input("Take a photo")
@@ -6,7 +7,8 @@ image = st.camera_input("Take a photo")
 if image is not None:
   bytes_data = image.getvalue()
   st.image(bytes_data)
-
+  loat_array = np.frombuffer(bytes_data, dtype=np.float32)
+  print(loat_array)
 
 
 
