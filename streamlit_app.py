ae2bdd90cfbf37b56e925e75ae41c655d7d52ff8
HyperFun <hoy.meng.phoon@email.com>
2024-10-16 16:35:36 +0800

    temp

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title("♻️ Welcome to our responsible helper app ✅")
 image = st.camera_input("Take a photo")
 if image is not None:
     time.sleep(3)
-    bytes_data = st.session_state['image'].getvalue()
+    bytes_data = image.getvalue()
     st.image(bytes_data)
     float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
     st.write(float_array)
