2038e19d95484ebb2ea6514f37f07e717318a9ba
HyperFun <hoy.meng.phoon@email.com>
2024-10-16 16:16:26 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import numpy as np
 import random
+
+
+if 'count' not in st.session_state:
+    st.session_state['count'] = 0
+
 st.title("♻️ Welcome to our responsible helper app ✅")
 image = st.camera_input("Take a photo")
 
-v = 10
-st.write(v)
 if image is not None:
   bytes_data = image.getvalue()
   st.image(bytes_data)
   float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
   st.write(float_array)
+  st.session_state['count'] += 1
+
+st.write(st.session_state['count'])
   
-
-
-
-
 
 
 
