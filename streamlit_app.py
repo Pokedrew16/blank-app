289f5118bba45d498d95b1bf5ae9a9bc9283ee51
HyperFun <hoy.meng.phoon@email.com>
2024-10-16 17:04:27 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ image = st.camera_input("Take a photo")
 if image is not None:
     bytes_data = image.getvalue()
     st.image(bytes_data)
-    float_arr = np.array(bytes_data, dtype=np.float32)
+    float_arr = np.array(bytes_data, dtype=np.float)
     st.write(type(float_arr))
 
 
