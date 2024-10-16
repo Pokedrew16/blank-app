ae85e9929aacffe01af9bc21b10bc43d484a4d30
HyperFun <hoy.meng.phoon@email.com>
2024-10-16 16:33:51 +0800

    temp

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,17 @@ import random
 import time
 
 
-if 'image' not in st.session_state:
-    st.session_state['image'] = None
+#if 'image' not in st.session_state:
+#    st.session_state['image'] = None
 
 st.title("♻️ Welcome to our responsible helper app ✅")
-if st.session_state['image'] is None:
-    st.session_state['image'] = st.camera_input("Take a photo")
-else:
-    bytes_data = st.session_state['image'].getvalue()
-    st.image(bytes_data)
-    float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
-    st.write(float_array)
+image = st.camera_input("Take a photo")
+#bytes_data = st.session_state['image'].getvalue()
+#st.image(bytes_data)
+#float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
+#st.write(float_array)
+
+st.write(image is None)
 
 
 
