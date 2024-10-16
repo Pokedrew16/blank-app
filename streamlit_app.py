2f5a26f62f52507017af198d4754b4f9d735f262
HyperFun <hoy.meng.phoon@email.com>
2024-10-16 16:31:00 +0800

    temp

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import time
 
 
 if 'count' not in st.session_state:
-    st.session_state['frameTemp'] = 0
     st.session_state['image'] = None
 
 st.title("♻️ Welcome to our responsible helper app ✅")
@@ -13,16 +12,11 @@ st.title("♻️ Welcome to our responsible helper app ✅")
 if st.session_state['image'] is None:
     st.session_state['image'] = st.camera_input("Take a photo")
 else:
-    if st.session_state['frameTemp'] != 0:
-        bytes_data = st.session_state['image'].getvalue()
-        st.image(bytes_data)
-        float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
-        st.write(float_array)
-        st.session_state['frameTemp'] = 0
-    st.session_state['frameTemp'] += 1
+    bytes_data = st.session_state['image'].getvalue()
+    st.image(bytes_data)
+    float_array = np.frombuffer(bytes_data, dtype=np.float32)/255.0
+    st.write(float_array)
 
-st.write(st.session_state['frameTemp'])
-  
 
 
 
