ff37e71411d9dc86a4bb93e2273d8f48271e1a00
HyperFun <hoy.meng.phoon@email.com>
2024-10-23 16:46:11 +0800

    test

--- image_rec.py
+++ image_rec.py
@@ -23,8 +23,7 @@ def load_database():
 def process_img(img):
     if 'temp_img' not in st.session_state:
         array = np.array(Image.open(img))
-        st.write(array.shape)
-        (count, width, height, channel) = array.shape
+        (height, width, channel) = array.shape
         for x in range(0, width):
             for y in range(0, height):
                 array[0, x, y, 0] = 0.0
