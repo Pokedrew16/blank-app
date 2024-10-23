fdef26112809c049dd4741a367ea73f434bb5cf0
HyperFun <hoy.meng.phoon@email.com>
2024-10-23 15:36:25 +0800

    database

--- image_rec.py
+++ image_rec.py
@@ -1,9 +1,19 @@
 import tensorflow as tf
 import streamlit as st
 import numpy as np
+    
+class Item:
+    def __init__(self, name_v = "None", link_v = "www.amazon.com/", img_v = "none.png"):
+        self.name = name_v
+        self.link = link_v
+        self.image_name = img_v
 
 def load_database():
-    with open(f"./data/helloworld.txt", "r") as file:
-        return file.read()
-
+    item_arr = []
+    with open("./data/helloworld.txt", "r") as file:
+        strs = file.readlines()
+        for s in strs:
+            entry_arr = s.split(",")
+            item_arr.append(Item(entry_arr[0].strip(), entry_arr[1].strip(), entry_arr[2].strip()))
+    return item_arr
 
