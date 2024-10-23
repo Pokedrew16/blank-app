22a9965acca07c7b1a8526e1f0d56da7a6bb07d9
junyanaerospace@gmail.com <junyanaerospace@gmail.com>
2024-10-23 19:12:41 +0800

    test

--- image_rec.py
+++ image_rec.py
@@ -1,41 +1,8 @@
 import tensorflow as tf
 import streamlit as st
+import image_data as id
 import numpy as np
 
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
     
-class Item:
-    def __init__(self, name_v = "None", link_v = "www.amazon.com/", img_v = "none.png"):
-        self.name = name_v
-        self.link = link_v
-        self.image_name = img_v
-
-    def display(self):
-        st.subheader("Common Name: " + self.name)
-        st.image("./data/" + self.image_name)
-        st.caption("This product may be bought online at: " + self.link)
-
-def load_database():
-    item_arr = []
-    with open("./data/database.txt", "r") as file:
-        strs = file.readlines()
-        for s in strs:
-            entry_arr = s.split(",")
-            item_arr.append(Item(entry_arr[0].strip(), entry_arr[1].strip(), entry_arr[2].strip()))
-    return item_arr
-
-def to_numpy_img_conventional_axes(img):
-    return np.array(Image.open(img)).transpose((1,0,2)) if (img is not None) else None
-
-def run_over_all_pixels_np(func, np_arr_inout):
-    if (np_arr_inout is None):
-        return
-
-    (width, height, channel) = np_arr_inout.shape
-    for x in range(width):
-        for y in range(height):
-            func(np_arr_inout, x, y)
-
-def display_numpy_img_conventional_axes(np_arr):
-    st.image(np_arr.transpose((1,0,2)))
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import image_data as id
 import image_rec as ir
 import streamlit as st
 import numpy as np
@@ -9,15 +10,15 @@ def invert_colour(np_arr, x, y):
     np_arr[x,y,2] = 1.0 - np_arr[x,y,2];
 
 if 'database' not in st.session_state:
-    st.session_state['database'] = ir.load_database()
+    st.session_state['database'] = id.load_database()
 
 st.title("♻️ Welcome to our responsible helper app ✅")
 image = st.camera_input("Take a photo")
 
 if image is not None:
-    temp = ir.to_numpy_img_conventional_axes(image)
-    ir.run_over_all_pixels_np(invert_colour, temp)
-    ir.display_numpy_img_conventional_axes(temp)
+    temp = id.to_numpy_img_conventional_axes(image)
+    id.run_over_all_pixels_np(invert_colour, temp)
+    id.display_numpy_img_conventional_axes(temp)
 
     for i in st.session_state['database']:
         i.display()
