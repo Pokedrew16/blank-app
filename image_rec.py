f00f607f394c522d9b0c188bae0e6c6dcf572501
Andrew <116625003+Pokedrew16@users.noreply.github.com>
2024-10-23 15:11:08 +0800

    Update image_rec.py

--- image_rec.py
+++ image_rec.py
@@ -3,5 +3,7 @@ import streamlit as st
 import numpy as np
 
 def load_database():
-    with open("./data/helloworld.txt", "r") as file:
+    with open(f"./data/{object}.txt", "r") as file:
         return file.read()
+
+object = "helloworld"
