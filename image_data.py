9226b3a9b02e5b173f45b564fa6da6185c01bcef
junyanaerospace@gmail.com <junyanaerospace@gmail.com>
2024-11-08 20:11:09 +0800

    test

--- image_data.py
+++ image_data.py
@@ -3,13 +3,16 @@ import numpy as np
 from PIL import Image, ImageOps
     
 class Item:
-    def __init__(self, name_v = "None", link_v = "www.amazon.com/", img_v = "none.png"):
+    def __init__(self, name_v = "None", message_v = "",  link_v = "www.amazon.com/", img_v = "none.png"):
         self.name = name_v
+        self.message = message_v
         self.link = link_v
         self.image_name = img_v
 
     def display(self):
-        st.subheader("Common Name: " + self.name)
+        st.header("Common Name: " + self.name, divider="gray")
+        if self.message is not "":
+            st.subheader(self.message)
         st.image("./data/" + self.image_name)
         st.caption("This product may be bought online at: " + self.link)
 
@@ -22,7 +25,7 @@ def load_database():
         item_arr.append(copy)
         for s in strs:
             entry_arr = s.split(",")
-            item_arr.append(Item(entry_arr[0].strip(), entry_arr[1].strip(), entry_arr[2].strip()))
+            item_arr.append(Item(entry_arr[0].strip(), entry_arr[1].strip(), entry_arr[2].strip(), entry_arr[3].strip()))
     return item_arr
 
 def to_model_img(img):
